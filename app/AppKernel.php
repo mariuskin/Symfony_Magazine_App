b32b8937be222d1759d13dcede37505aa0584d0f
Marius <marius.kinderis@hotmail.fr>
2016-07-20 15:43:41 +0300

    Donloaded Assetic and added bootstrap and jquery in the project using it. Important to note: check the official documentation to see how it is done to configure Assetic.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new LG\MagazineBundle\LGMagazineBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
